999113c9f67666833b838d83d8235bdf09cf8c8b
GabrieldeMoura020 <109754818+GabrieldeMoura020@users.noreply.github.com>
2022-11-16 21:32:45 -0300

    Add files via upload

---
+++ ServicoInternet.java
@@ -0,0 +1,26 @@
+public class ServicoInternet implements Vendavel {
+
+    private String descricao;
+    private Double valorUnitario;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(Double valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    @Override
+    public Double getValorVenda() {
+        return valorUnitario;
+    }
+}
---
+++ Vendavel.java
@@ -0,0 +1,6 @@
+public interface Vendavel {
+
+    Double getValorVenda();
+
+
+}
